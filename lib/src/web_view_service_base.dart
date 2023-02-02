ce8027ee461a9d7d8bfe4a26caa24c8b84c761e5
Grayson Erhard <develop@graysonerhard.com>
2023-02-02 11:03:15 -0700

    updated default whiteListedUrls to contain social media urls

--- lib/src/web_view_service_base.dart
+++ lib/src/web_view_service_base.dart
@@ -1,5 +1,19 @@
 class WebViewService {
-  final List<String> whiteListedUrls;
+  List<String> whiteListedUrls = [
+    'https://facebook.com',
+    'https://www.instagram.com',
+    'https://twitter.com',
+    'https://youtube.com',
+    'https://linkedin.com',
+    'https://tiktok.com',
+    'https://youtu.be',
+    'https://www.youtube.com',
+    'https://youtube.com',
+    'https://vimeo.com',
+    'https://player.vimeo.com',
+  ];
 
-  WebViewService({required this.whiteListedUrls});
+  WebViewService({List<String> whiteListedUrls = const []}) {
+    this.whiteListedUrls.addAll(whiteListedUrls);
+  }
 }
